2703ed9be0d054c95922cda0e9b0422f527b9eee
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-27 10:10:29 +0100

    [REFACTOR] Removed unnecessary code
    It turns out Screen does not need any form of equility (for the test)
    As long as the test are passing, remove code.

--- lib/swimlane/screen.rb
+++ lib/swimlane/screen.rb
@@ -7,18 +7,6 @@ class Screen
   def renderOn(brush)
     brush.render :screen, @description
   end
-
-  def ==(screen)
-    self.eql?(screen)
-  end
-
-  def eql?(screen)
-    self.description == screen.description
-  end
-
-  def hash
-    @description.hash
-  end
 end
 
 class NullScreen
