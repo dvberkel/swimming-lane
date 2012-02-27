68ac5b10aad3d45444936fae439e4ce18943b6b8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-27 13:21:39 +0100

    Transition introduced
    A NullTransition is introduced.

--- lib/swimlane/lane.rb
+++ lib/swimlane/lane.rb
@@ -1,10 +1,12 @@
 require 'screen'
+require 'transition'
 
 class Lane
-  attr_reader :name, :screen
+  attr_reader :name, :screen, :transition
   def initialize(name, screen = NullScreen.new)
     @name = name
     @screen = screen
+    @transition = NullTransition.new
   end
 
   def renderOn(brush)
