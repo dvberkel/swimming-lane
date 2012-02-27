2a16ecd872ed5d63f09e4aecc09dee7a4f9cb2b5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-27 14:48:31 +0100

    travis-lint task
    A travis-lint task is added to the rake file.

--- Rakefile.rb
+++ Rakefile.rb
@@ -15,3 +15,8 @@ RSpec::Core::RakeTask.new('rspec') do |task|
   task.rspec_opts = ['-I lib/swimlane']
   task.pattern = 'spec/**/*_spec.rb'
 end
+
+desc "Run travis-lint on .travis.yml"
+task :lint do
+  system("bundle exec travis-lint")
+end
