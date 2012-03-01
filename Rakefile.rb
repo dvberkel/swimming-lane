46e2d36ac591f3319bd6da7b4f07b1df53cb24e7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-01 09:20:58 +0100

    Generate sources
    The sources which treetop generates from the grammars is generated
    before the test phase begins. This ensures that tests always see
    the latest version of the test.

--- Rakefile.rb
+++ Rakefile.rb
@@ -6,13 +6,19 @@ require 'rspec/core/rake_task'
 task :default => [:test]
 
 desc "Run all test tasks"
-task :test => [:rspec] do
+task :test => [:generate, :rspec] do
   puts "Testing stage finished."
+end
+
+desc "generate sources from grammars"
+task :generate do
+  system("bundle exec tt lib/swimlane/dsl/*.treetop")
+  raise "generation of sources failed!" unless $?.exitstatus == 0
 end
 
 desc "Run rspec on the spec directory"
 RSpec::Core::RakeTask.new('rspec') do |task|
-  task.rspec_opts = ['-I lib/swimlane']
+  task.rspec_opts = ['-I lib/swimlane', '-I lib/swimlane/dsl']
   task.pattern = 'spec/**/*_spec.rb'
 end
 
